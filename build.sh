907eec838e91a1e073b9bfaa0f324a9d7851c40a
joseph <joseph@kali.josefir.com>
2022-04-13 19:39:22 +0200

    Edited build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,15 @@
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
+
+mkdir bin 
+mkdir build 
+mkdir ./build/boot 
+mkdir ./build/idt 
+mkdir ./build/io 
+mkdir ./build/memory 
+mkdir ./build/memory/heap
+mkdir ./build/memory/paging
+
+echo "\e[34m####### Start compiling #######\e[0m"
 make all
